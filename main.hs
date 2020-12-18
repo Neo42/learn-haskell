621b3cda5b36a8d76445e24435bfeae4d8eb69d5
Jiang Hao <giovannilance94@gmail.com>
2020-12-18 23:50:02 +0800

    Committed with Git-Upload

--- main.hs
+++ main.hs
@@ -1,3 +1,9 @@
+-- 1. brew install ghc
+-- 2. brew install cabal-install
+-- 3. Download Haskell extension on VSCode
+-- 4. Download Haskell Language server, Haskell Language Server wrapper on GitHub
+-- And you're all set!
+
 x = 1
 
 main = putStrLn "Hello"
